ae04f45f5bfad20cef8590378d5ba534fb781d17
Nicolas Escalante <nlante@gmail.com>
2016-07-01 17:26:42 -0300

    initial code

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# git-rev CLI
+
+CLI that uses [git-rev-sync](https://github.com/kurttheviking/git-rev-sync) to create a file with git rev info
+
+## Install
+
+```
+npm install git-rev-cli -g
+```
+
+## Usage
+
+From a git repo
+
+```
+git-rev VERSION
+```
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const argv = require('yargs').argv;
+const git = require('git-rev-sync');
+const files = argv._;
+const bluebird = require('bluebird');
+const fs = require('fs');
+
+bluebird.promisifyAll(fs);
+
+module.exports = bluebird.map(files, (filePath) => {
+  const json = JSON.stringify({
+    commit: git.long(),
+    tag:    git.tag() || '',
+    branch: git.branch() || '',
+    date:   new Date().toISOString()
+  }, null, 2);
+
+  return fs.writeFileAsync(filePath, json, 'utf8');
+});
